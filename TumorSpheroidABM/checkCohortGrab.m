9673082e1b4f50b717ae1e0c3035748839008c9c
Daniel Bergman <danielrbergman@gmail.com>
2023-03-31 15:42:21 -0400

    now using myfunctions for simCohort and setField and updateCohortTimer

--- TumorSpheroidABM/checkCohortGrab.m
+++ TumorSpheroidABM/checkCohortGrab.m
@@ -1,4 +1,4 @@
-function checkCohortGrab(cohort_name)
+function unequal = checkCohortGrab(cohort_name)
 
 start_wait = tic;
 filename = sprintf("data/cohort_%s/output.mat",cohort_name);
@@ -12,12 +12,15 @@ C = load(filename);
 
 C.ids = reshape(C.ids,[],C.nsamps_per_condition);
 
+
 idx = cell(length(C.cohort_size),1);
+unequal = string([]);
 for i = 1:size(C.ids,1)
 
     [idx{:}] = ind2sub(C.cohort_size,i);
     for j = 1:C.nsamps_per_condition
         S = load(sprintf("data/sims/%s/output_constants.mat",C.ids(i,j)));
+        stop_with_this_sim = false;
         for k = 1:numel(C.lattice_parameters)
             if iscell(C.lattice_parameters(k).path)
                 for l = 1:numel(C.lattice_parameters(k).path)
@@ -25,7 +28,14 @@ for i = 1:size(C.ids,1)
                     for m = 1:length(C.lattice_parameters(k).path{l})
                         s = s.(C.lattice_parameters(k).path{l}(m));
                     end
-                    assert(isequal(C.lattice_parameters(k).values(idx{k},l),s))
+                    if ~isequal(C.lattice_parameters(k).values(idx{k},l),s)
+                        unequal(end+1) = C.ids(i,j);
+                        stop_with_this_sim = true;
+                        break;
+                    end
+                end
+                if stop_with_this_sim
+                    break;
                 end
 
             else
@@ -33,7 +43,11 @@ for i = 1:size(C.ids,1)
                 for l = 1:length(C.lattice_parameters(k).path)
                     s = s.(C.lattice_parameters(k).path(l));
                 end
-                assert(isequal(C.lattice_parameters(k).values(idx{k}),s))
+
+                if ~isequal(C.lattice_parameters(k).values(idx{k}),s)
+                    unequal(end+1) = C.ids(i,j);
+                    break;
+                end
             end
 
         end
