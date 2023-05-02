229f298124cfe528453ab364fda4a2407fdd092b
Daniel Bergman <danielrbergman@gmail.com>
2023-05-02 16:48:37 -0400

    continuing to try to work out ABM sampling

--- TumorSpheroidABM/OxStudyControl/SampleABM/TestDrawUsingBarycentric.m
+++ TumorSpheroidABM/OxStudyControl/SampleABM/TestDrawUsingBarycentric.m
@@ -1,5 +1,6 @@
 % A script to test drawing from ABM parameter space
 
+addpath("~/Documents/MATLAB/myfunctions/")
 clearvars
 
 b1 = 2;
@@ -25,32 +26,38 @@ axis([0 1 0 1])
 % end
 
 %% n = 2 and b2 = 0
-A1 = asinh(u*sinh(b1))./b1;
+LHS = @(x) b1*(1-exp(-b1*x)) + exp(-b1*x)*(b1*x+1)-1;
+RHS = @(x,v) v*(b1+exp(-b1)-1);
+A1 = zeros(n,1);
+for i = 1:n
+    A1(i) = fsolve(@(x) LHS(x)-RHS(x,u(i)),0.5);
+end
 figureOnRight;
 plot(A1,u,"Marker",'.')
 axis([0 1 0 1])
 
 %% n = 2
 A1 = zeros(n,1);
-b2 = 1e-5;
+b2 = 1e-6;
 g = b2-b1;
 LHS = @(x) -g*exp(-b1*x) - b1*exp(g*x-b2);
-J = @(x) -b1*g*(exp(-b1*x)+exp(g*x-b2));
-RHS = @(x,v) v*(g*(1-exp(-b1)) + b1*(exp(-b2)-exp(-b1))) - g - b1*exp(-b2);
+J = @(x) b1*g*(exp(-b1*x)-exp(g*x-b2));
+RHS = u*(g*(1-exp(-b1)) + b1*(exp(-b2)-exp(-b1))) - g - b1*exp(-b2);
 K = g*(1-exp(-b1)) + b1*(exp(-b2)-exp(-b1));
-f = @(x,v) disperse([RHS(x,v)-LHS(x),J(x)]);
+f = @(x,R) disperse([LHS(x)-R,J(x)]);
 % f = @(x,u) (b1-b2)*(exp(-b1*x)-1) + b1*(exp(-b2) - exp((b2-b1)*x-b2)) - u*((1-exp(-b1))*(b2-b1)+b1*(exp(-b2)-exp(-b1)));
 % f = @(x,u) (b1-b2)*(exp(b1*x)*(1-exp(b1*(1-x))*u)-1+u) + b1*(exp(-b2) - exp((b2-b1)*x-b2))  - u*b1*(exp(-b2)-exp(-b1));
-opts = optimoptions("fsolve","Display","iter-detailed","MaxFunctionEvaluations",1e4,"FunctionTolerance",1e-20,"OptimalityTolerance",1e-10,'SpecifyObjectiveGradient',true,"StepTolerance",1e-9,'OutputFcn', @outfun);
-figureOnRight;
+opts = optimoptions("fsolve","Display","iter-detailed","MaxFunctionEvaluations",1e4,"FunctionTolerance",1e-20,"OptimalityTolerance",1e-16,'SpecifyObjectiveGradient',true,"StepTolerance",1e-9);%,'OutputFcn', @outfun);
+% figureOnRight;
 for i = 1:n
-    clf;
+    % clf;
     if i==1
         A0 = 0;
     else
-        A0 = A1(i-1,1)-K*du;
+        A0 = min(1,max(0,A1(i-1,1)-K*du));
     end
-    A1(i) = fsolve(@(x) f(x,u(i)),A0,opts);
+    R = RHS(i);
+    A1(i) = fsolve(@(x) f(x,R),A0,opts);
 end
 
 A2 = -log(1-(1-exp(-b2*A1)).*u)/b2;
