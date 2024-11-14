8d895cdc3b8448cf4b2cc7f90c19a33a4f67494d
drbergman <danielrbergman@gmail.com>
2024-11-13 19:47:27 -0500

    removing unnecessary files

--- BernardoProfileWork/vbmodel_eval.m
+++
@@ -1,8 +0,0 @@
-% Van der ... model
-
-function y = vbmodel_eval(pset, x0, times)
-            
-    options = odeset('AbsTol',1e-6,'RelTol',1e-8);
-    [~,y] = ode15s(@(t,x) vbmodel(x,pset),times,x0,options); % solve ODE systems
-        
-end
